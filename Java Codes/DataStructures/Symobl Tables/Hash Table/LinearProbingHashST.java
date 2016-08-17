abe98d09e7e586f6e71b6b70417c4774e683dba6
MT <cyberaxx@users.noreply.github.com>
2016-08-16 19:25:18 -0700

    Liner probing added to HST.

---
+++ Java Codes/DataStructures/Symobl Tables/Hash Table/LinearProbingHashST.java
@@ -0,0 +1,14 @@
+import java.util.Iterator;
+
+public class LinearProbingHashST<Key, Value> implements Iterable<Key> {
+
+
+
+  /*
+    in order to implement java Iterable interface
+    we must override its abstract method iterator:
+  */
+  @Override
+  public Iterator<Key> iterator() {return null;}
+
+}
