7913328bb9e5a2f4975dbe7b7214561e89aa5971
MT <cyberaxx@users.noreply.github.com>
2016-08-17 23:11:17 -0700

    TopM.java toy example for java.util.PriorityQueue, finalized.

--- Java Codes/DataStructures/Priority Queue/TopM.java
+++ Java Codes/DataStructures/Priority Queue/TopM.java
@@ -1,8 +1,53 @@
 import java.util.Collection;
 import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.Arrays;
 
+/*
+Find the largest M items in a stream of N items.
+*/
 
 public class TopM {
-  public static <Key> PriorityQueue<Key> topM(Comparable [] items, int m) {return null;}
-  public static void main(String [] args) {}
+  public static void main(String [] args) {
+    // Find the largest M items in a stream of N items.
+    int M=15;
+    Integer [] items=new Integer[10000];
+    int N=items.length;
+    Random randomEntry=new Random();
+    Random randomRepetition=new Random();
+    int maxInt=9999999;
+    Integer entry;
+    int repetition;
+
+    // Instantiate form java MinPQ: java.util.ProrityQueue:
+    PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+
+    // generate a random stream of N items of Comparable type (Intergers):
+    for(int i=0; i<items.length;i++) {
+      entry=randomEntry.nextInt(maxInt);
+      repetition=randomRepetition.nextInt(4-1)+1; // [1 4)
+      for(int j=0; j<=repetition; j++) {
+        /* To maintain top M items:
+	   Check if the Min Oriented Priority Queue contains less than M item
+	      a. If so, add the new item to the Collection type
+	      b. if not:
+                 1. remove the Min from the current Collection
+		 2. add the new item to the collection
+	*/
+
+        if(pq.size()<M)  pq.offer(entry); // O(logM)
+        else {
+  	  // remove smallest items from the MinPQ to make some room for new items
+          while(pq.size()>=M)  pq.poll(); // O(logM)
+          pq.offer(entry);
+        }
+      }
+    } 
+        
+    System.out.print(Arrays.toString(pq.toArray()));
+    System.out.println();
+
+    System.out.print(pq);
+    System.out.println();
+  }
 }
