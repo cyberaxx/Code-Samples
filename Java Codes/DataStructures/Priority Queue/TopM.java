bca0458067d38211f2009efe93c0a6bdc39ef221
MT <cyberaxx@users.noreply.github.com>
2016-08-17 22:42:49 -0700

    TopM.java file added to Priority Queue folder.

---
+++ Java Codes/DataStructures/Priority Queue/TopM.java
@@ -0,0 +1,8 @@
+import java.util.Collection;
+import java.util.PriorityQueue;
+
+
+public class TopM {
+  public static <Key> PriorityQueue<Key> topM(Comparable [] items, int m) {return null;}
+  public static void main(String [] args) {}
+}
