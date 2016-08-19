511d5d9fe90c970fe2f90fdb7405da50fb30814f
MT <cyberaxx@users.noreply.github.com>
2016-08-18 23:27:57 -0700

    BST verification revised.

--- Java Codes/DataStructures/Symobl Tables/BST/Verfication.java
+++ Java Codes/DataStructures/Symobl Tables/BST/Verfication.java
@@ -1,3 +1,16 @@
+/*
+https://leetcode.com/problems/validate-binary-search-tree/
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+*/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -7,37 +20,40 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+
+public class Verification {
     public boolean isValidBST(TreeNode root) {
+        // extreme test case: empty tree is a BST
         if(root==null) return true;
-        return validate(root, root.val, root.val);
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    private static boolean validate(TreeNode root, int min, int max) {
-        // BASE CASE: 1. EMPTY TREE,  TREE WITH ONE NODE
-        if(root==null)  return true; // 0
-        if(root.left==null && root.right==null) return true; // 1
+    // helper method to validate BST recursively
+    private static boolean validate(TreeNode root, long min, long max) {
+        // BASE CASE: where to stop division:
+        // 1. EMPTY TREE
+        // 2. TREE WITH 1 NODE
+        if(root==null) return true;// 0 node is BST
+        if(root.left==null && root.right==null) return true; // 1 node is a BST
         
-        //RECURRENCE:
-        // only has LEFT subtree: work with max
+        // RECURRENCE:
+        // 1. only left subtree is NOT null
         if(root.right==null) {
-            if(root.val<=root.left.val || root.left.val>=min)  return false;
-            // update the min
-            min=root.left.val;
-            return validate(root.left, min, root.val);
+            // check if BST condition is violated:
+            if(root.val<=root.left.val || (long)root.left.val<=min) return false;
+            // update max
+            return validate(root.left, min, (long)root.val);
         }
-        // only has RIGHT subtree:
+        // 2. only right subtree is NOT null
         if(root.left==null) {
-            if(root.val>=root.right.val || root.right.val<=max)  return false;
-            // update the max
-            max=root.right.val;
-            return validate(root.right, root.val, max);
+            // check if BST condition is violated:
+            if(root.val>=root.right.val || (long)root.right.val>=max) return false;
+            // update min
+            return validate(root.right, (long)root.val, max);
         }
-        // bothe left and right subtree
-        else {
-            if(root.val<=root.left.val || root.val>=root.right.val || root.left.val>=min || root.right.val<=max)  return false;
-            min=root.left.val;
-            max=root.right.val;
-            return validate(root.left, min, root.val) && validate(root.right, root.val, max);
-        }
+        
+        // 3. have both left and right subtrees are not null:
+        // check if BST condition is violated:
+        if(root.val<=root.left.val || root.val>=root.right.val || (long)root.right.val>=max || (long)root.left.val<=min) return false;
+        return validate(root.left, min, (long)root.val) && validate(root.right, (long)root.val, max);
     }
 }
