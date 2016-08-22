a9422c2562768a424b1848f16df4fa5bd35410fb
MT <cyberaxx@users.noreply.github.com>
2016-08-22 05:11:49 -0700

    TaxiCab.java file added to Priority Queue folder

---
+++ Java Codes/DataStructures/Priority Queue/TaxiCab.java
@@ -0,0 +1,5 @@
+/*
+Taxicab numbers. 
+Write a program Taxicab.java that reads in a command line parameter N and prints out all nontrivial solutions of a^3 + b^3 = c^3 + d^3. 
+such that a, b, c, and d, are less than or equal to N.
+*/
