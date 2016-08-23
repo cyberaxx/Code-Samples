947fac418e80edb9bcb9aec63af43d373e61ea47
MT <cyberaxx@users.noreply.github.com>
2016-08-22 21:50:42 -0700

    MedianPQ added to Priority Queue repository

--- Java Codes/DataStructures/Priority Queue/MedianPQ.java
+++ Java Codes/DataStructures/Priority Queue/MedianPQ.java
@@ -1,12 +1,17 @@
 /*
 Dynamic-median finding:
 Design a data type that supports insert in logarithmic time, find the median in constant time, and remove the median in logarithmic time.
+
+http://algs4.cs.princeton.edu/24pq/
 */
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import java.util.Deque;
 import java.util.ArrayDeque;
+
+import java.util.Arrays;
 
 // MedianPQ is a Collection data type which contain generic keys that have total ordering:
 // It's type prameter is Key
@@ -31,11 +36,33 @@ public class MedianPQ<Key extends Comparable<Key>> implements Iterable<Key> {
   }
 
   public MedianPQ(Key[] inputArray) {}
+
   // Instance methods:
   // API: insert/delete/isEmpty/size/min/max/toArray
 
   // Add a key to an instance of MedianPQ:
-  public void add(Key key){}
+  public void add(Key key) {
+    // OVERFLOW: check if array is not already full:
+    if(N==items.length-1)  resize(2*items.length);
+    
+    // Max, Min update:
+    if(isEmpty()) {
+      maxStack.push(key);
+      minStack.push(key);
+    }
+    //MedianPQ instance is not empty compare the curren key with the top of Min, Max stacks
+    else {
+      // if key "is less than or equal to the min (minStack.peek() item) push it to the min stack
+      if(key.compareTo(minStack.peek())<=0)  minStack.push(key);
+      // if key "is greater than or equal to the max (maxStack.peek() item) push it to the max stack
+      if(key.compareTo(maxStack.peek())>=0)  maxStack.push(key);
+    }
+
+    // add the new item to the end of MedianPQ instance array:
+    items[++N]=key;
+    // swim it up to its rightful level of competence:
+    swim(N);
+  }
 
   // delete Median for an instance of a MedianPQ:
   public Key delMedian(){return null;}
@@ -91,10 +118,10 @@ public class MedianPQ<Key extends Comparable<Key>> implements Iterable<Key> {
   
   // Sink and swim operations:
   // Sink down a new root to its rightful level of competence:
-  private void sink(int k) {}
- 
+  private void sink(int k){} 
+
   // Swim up the new added node to its rightful level of competence:
-  private void swim(int k){}
+  private void swim(int k) {}
 
   // generic comparison and swap method
   private boolean less(int i, int j) {
@@ -103,7 +130,7 @@ public class MedianPQ<Key extends Comparable<Key>> implements Iterable<Key> {
   private boolean greater(int i, int j) {
     return items[i].compareTo(items[j])>0;
   }
-  private void exchange(int i, int j) {
+  private void exch(int i, int j) {
     Key temp=items[i];
     items[i]=items[j];
     items[j]=temp;
@@ -120,4 +147,17 @@ public class MedianPQ<Key extends Comparable<Key>> implements Iterable<Key> {
    to iterate over items within the Median Priority Queue Collection typ:
   */
    public Iterator<Key> iterator(){return null;} 
+
+
+  // test client:
+  public static void main(String[] args) {
+    MedianPQ<Integer> pq=new MedianPQ<Integer>();
+    for(int i=0; i<11; i++) pq.add(i);
+    System.out.println("Min: " + pq.min());
+    System.out.println("Max: " + pq.max());
+    System.out.println("Median: " + pq.median());
+    System.out.print(Arrays.toString(pq.items));
+    System.out.println();
+  }
+
 }
