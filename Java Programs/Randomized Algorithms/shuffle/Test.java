4da43dc7fd6fcb8af8d60c8cf8ab353627adb8c1
MT <cyberaxx@users.noreply.github.com>
2016-08-23 22:23:38 -0700

    removed duplicate folders (renaming problem)

--- Java Programs/Randomized Algorithms/shuffle/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-  public static void testmessage() {
-    System.out.println("Hello, World!");
-  }
-}
--- Java Programs/Randomized Algorithms/shuffle/TestLauncher.java
+++
@@ -1,7 +0,0 @@
-public class TestLauncher {
-  public static void main (String [] args) {
-    Test.testmessage();
-    for(int i=0; i<args.length; i++)  System.out.print(args[i] + " ");
-    System.out.println();
-  }
-}
