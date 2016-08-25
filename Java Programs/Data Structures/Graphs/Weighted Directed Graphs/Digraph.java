3d9109c92b4e7e80f3d081a575d2760576745423
MT <cyberaxx@users.noreply.github.com>
2016-08-25 16:09:34 -0700

    Weighted Directed Graph API added to Graph repo

---
+++ Java Programs/Data Structures/Graphs/Weighted Directed Graphs/Digraph.java
@@ -0,0 +1,72 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Digraph{
+  // instance variables:
+  private final int V; // number of vertices
+  private int E; // number of edges
+  private int [] inDegree; // the indegree of all vertices of the graph
+  private List<Edge>[] adj; // adj list of the a graph instance
+
+  // Constructor:
+  public Digraph(int v) {
+    if(v<0) throw new IllegalArgumentException("Number of vertices of a Digraph instance must be non-negative!");
+    // initialize instance fields:
+    this.V=v;
+    this.E=0;
+    inDegree=new int[V];
+    adj=(List<Edge>)new List[V]; // UGLY CASTING
+    for(int i=0; i<V; i++)
+      adj[i]=new LinkedList<Edge>(); // instantiate a LinkedList collection of generic type Edge
+  }
+
+  public Digraph(Digraph G) {
+    this.V=G.V;
+    this.E=G.E;
+    this.inDegree=new int[V];
+    this.adj=(List<Edge>)new List[V]; // UGLY CASTING
+    for(int v=0; v<V; v++) {
+      this.inDegree[v]=G.inDegree(v);
+      this.adj[v]=new LinkedList<Edge>(G.adj(v));
+    }
+  }
+
+  // API:
+  // instance methods:
+  // validateVertex(v)
+  public int V(){return V;}
+  public int E(){return E;}
+  public int inDegree(int v){
+    validateVertex(v);
+    return inDegree[v];
+  }
+  public int outDegree(int v) {
+    validateVertex(v);
+    return adj[v].size();
+  }
+  public List<Edge> adj(int v){
+    validateVertex(v);
+    return adj[v];
+  }
+  public void addEdge(int from, int to, double weight){
+    validateVertex(from);
+    validateVertex(to);
+    if(Double.isNaN(weight)) throw new IllegalArgumentException();
+    Edge edge=new Edge(from, to, weight);
+    adj[from].add(edge);
+    inDegree[to]++;
+    E++;
+
+  }
+  public void addEdge(Edge e){
+  }
+
+
+  // helper methods:
+  // vertices must be integer within [0 V-1] range:
+  private void validateVertex(int v) {
+    if(v<0 || v>=V-1) throw new IndexOutOfBoundsException("The vertex index is out of legal bounds!");
+  }
+
+}
