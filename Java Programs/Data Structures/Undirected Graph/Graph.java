d44bcc6748a93abb0cd0a1b29acaa66e9f08e1d2
MT <cyberaxx@users.noreply.github.com>
2016-08-25 01:27:48 -0700

    Undirected Graph abstraction implemented.

--- Java Programs/Data Structures/Undirected Graph/Graph.java
+++ Java Programs/Data Structures/Undirected Graph/Graph.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 
 public class Graph {
   // instance variable:
-  private int V; // number of Vertices of the graph
+  private final int V; // number of Vertices of the graph
   private int E; // number of Edges of the graph
   private List<Integer>[] adj; // adjacency List of the Graph: private ArrayList<List<Integer>> adj
 
   // Constructor:
   public Graph(int v) {
+    // number of vertices can not be negative
+    if(v<0) throw new IllegalArgumentException();
+
     this.V=v;
     this.E=0;
+
     // instantiate the adjacency list:
     adj=(List<Integer>[]) new Object[V]; // UGLY CASTIN: no generic array creation
     for(int i=0; i<V; i++) {
       adj[v]=new LinkedList<Integer>();
+    } 
+  }
+
+  public Graph(Graph G) {
+
+    // initialize number of vertices and edges
+    this.V=G.V();
+    this.E=G.E();
+
+    // instantiate the adjacency list:
+    adj=(List<Integer>[]) new Object[V]; // UGLY CASTIN: no generic array creation
+    for(int i=0; i<V; i++) {
+      adj[v]=new LinkedList<Integer>(G.adj(v));
     } 
   }
 
@@ -24,13 +41,24 @@ public class Graph {
   public int V(){return V;}
   public int E(){return E;}
   public void addEdge(int v, int w) {
+    // validate vertices:
+    validateVertec(v);
+    validateVertec(w);
     // undirected graph: add v to the adj[w] and add w to adj[v]:
     adj[v].add(w); // adds w to the tail of DList adj[v] in constant time
     adj[w].add(v); // adds v to the tail of DList adj[w] in constant time
+    // update the edge count:
+    E++;
   }
 
   // returns an Iterable data type that represents all vertices adjacent to vertex v
   public List<Integer> adj(int v){return adj[v];}
+
+
+  // Helper methods:
+  private void validateVertex(int v){
+    if(v<0 || v>=this.V) throw new IllegalArgumentException();
+  }
 
   // static methods:
   public static int degree(Graph G, int v) {
@@ -40,6 +68,7 @@ public class Graph {
     return degree;
   }
 
+  // find the max degree among vertice of a given instance of Graph type
   public static int maxDegree(Graph G) {
     int maxDegree=degree(G, 0);
     for(int v=1; v<G.V(); v++)
@@ -47,6 +76,7 @@ public class Graph {
     return maxDegree;
   }
 
+  // find the min degree among vertice of a given instance of Graph type
   public static int minDegree(Graph G) {
     int minDegree=degree(G, 0);
     for(int v=1; v<G.V(); v++)
@@ -54,15 +84,14 @@ public class Graph {
     return minDegree;
   }
 
-  // Sum of degrees of all vertices in an undirected graph is 2*E (twice the number of edges because each edge counts twice (once from each end point)
+  // sum of degrees of all vertices in an undirected graph is 2*E (twice the number of edges because each edge counts twice (once from each end point)
   public static int averageDegree(Graph G) {return (2*G.E())/G.V();}
 
+  // count number of self loops in a given instance of Graph type
   public static int SelfLoopCount(Graph G) {
     int count=0;
     for(int v=0; v<G.V(); v++)
       if(G.adj(v).contains(v)) count++;
     return count;
   }
-
-  
 }
