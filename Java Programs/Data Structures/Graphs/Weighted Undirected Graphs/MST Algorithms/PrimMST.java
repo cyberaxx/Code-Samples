2cc762b210024673cf9680df50da7ab9093ea21c
MT <cyberaxx@users.noreply.github.com>
2016-08-26 22:07:58 -0700

    PrimMST.java skeleton implemented.

--- Java Programs/Data Structures/Graphs/Weighted Undirected Graphs/MST Algorithms/PrimMST.java
+++ Java Programs/Data Structures/Graphs/Weighted Undirected Graphs/MST Algorithms/PrimMST.java
@@ -1,4 +1,11 @@
 import edu.princeton.cs.algs4.IndexMinPQ;
 
 public class PrimMST{
+
+  // define two other top-level classes (Graph, Edge) here as a static nested classes (For packaging convenience)
+  private static class Graph{}
+  private static class Edge implements Comparable<Edge>{
+  @Override 
+  public int compareTo(Edge e){return 0;}  
+  }
 }
