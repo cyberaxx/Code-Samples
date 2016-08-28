a0070d9b3b10da29bfe88b301c12b30da236077b
MT <cyberaxx@users.noreply.github.com>
2016-08-28 09:20:46 -0700

    Integer knapsack implemented from scratch

--- Java Programs/Dynamic Programming/IntKnapsack/Test.java
+++ Java Programs/Dynamic Programming/IntKnapsack/Test.java
@@ -1,29 +1,32 @@
 public class Test {
   public static void main (String [] args) {
-    int [] vals = {3,3,4,2};
-    int [] weights = {5, 6, 10, 12};
-    int capacity = 23;
+    int [] vals = new int[]{3,3,4,2};
+    int [] weights = new int[]{5, 6, 10, 12}; // weights are integral
+    int capacity = 23; // capacity is integral
     System.out.println(intKnapsack(vals, weights, capacity));
   }
 
   public static int intKnapsack(int [] values, int [] weights, int capacity) {
-    int [][] optimalVal = new int [values.length+1][capacity+1]; // n*W matrix
-    // consider items being a seq of n items, in no particular order
+    // conside n items as a sequence 1 ... n with integer weights and values:
+    int n=values.length;
+    int[][] knapsack=new int[n+1][capacity+1]; // n and canpacity inclusive
 
-    // initial subproblems:
-    // 1. 0 capacity
-    for(int i=0; i<=optimalVal.length; i++) optimalVal[i][0]=0;
-    // 2. 0 items
-    for(int j=0; j<=optimalVal[0].length; j++) optimalVal[0][j]=0;
-  
-    // solutions to subproblems are prefixes of the optimal solutions
-    for (int s=1; s<=capacity; s++) {
-      for (int j=1; j<value.length; j++) {
-	// if item is larger than the capacity do Not take it
-        if(weights[i] > s) optimalVal[s][j] = optimalVal[s][j]; 
+    // BASE CASE: 1. 0 item, 2. capacity
+    // 1. 0 item, capacity from 0-capacity:
+    for(int j=0; j<=capacity; j++)  knapsack[0][j]=0;  
+    // 1. 0 capacity, items from 0-n:
+    for(int i=0; i<=n; i++)  knapsack[i][0]=0;  
+    
+    // RECURRENCE: bottom up
+    for(int j=1; j<=capacity; j++) { // considering remaining capacity from 1 ... capacity
+      for(int i=1; i<=n; i++) {// considering 1 ... n items
+        // check if the weight of the item i is greater than remaining capacity:
+        int w=weights[i-1]; // weights matrix is 0-based index
+        int v=values[i-1]; // values matrix is 0-based index
+        if(w>j)  knapsack[i][j]=knapsack[i-1][j]; // Do NOT take item i, and remaining space stays the same
+        else  knapsack[i][j]=Math.max(knapsack[i-1][j],knapsack[i-1][j-w]+v);
       }
     }
+    return knapsack[n][capacity];
   }
-
-
 }
