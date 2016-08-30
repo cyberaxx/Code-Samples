4ca35bf19b4d5f10814d817d65bc335ae59d6f33
MT <cyberaxx@users.noreply.github.com>
2016-08-30 05:24:25 -0700

    Binary Search Array implementation of Symbol Table interface with comparable type keys

--- Java Programs/Data Structures/Symobl Tables/BSA/BSA.java
+++ Java Programs/Data Structures/Symobl Tables/BSA/BSA.java
@@ -1,1 +1,39 @@
 /* Binary Search Array Data Structure - Implements ST interface using resizable parallel arrays to maintain a collection key-value pairs. */
+
+// This class implements Symbol Table interface: ST<Key,Value> for keys with total order
+public class BSA<Key extends Comparable<Key>, Value> {
+
+  // instance variables:
+  // parallel resizable arrays to maintain key-value pairs:
+  private Key[] keys; // generic array of keys
+  private Value[] values; // generic array values
+  private int size;
+
+  // Constructor:
+  public BSA() {
+    // initialize instance variables:
+    size=0;
+    keys=(Key[]) new Comparable[16]; // UGLY CASTING: java does not allow generic array creation
+    values=(Value[]) new Object[16]; // UGLY CASTING: java does not allow generic array creation
+  }
+
+  // API: ST operations: put(key,val), get(key), delete(key), contains(key), size(), isEmpty()
+
+  // Ordered operation for Comparable keys: min, max, delMin, delMax, floor(key), ceiling(key), successor(key), predeccessor(key), select(k), rank(key), keys(),  
+  
+  // helper methods:
+  // resize: grow and shrink the Symbol Table as required:
+  private void resize(int capacity) {
+    // Create a new pair of parallel arrays of new capacity:
+    Key[] k=(Key[]) new Comparable[capacity];
+    Value[] v=(Value[]) new Object[capacity];
+    // copy keys and values over (for hashing this part is completely different and it requires re-hashing of all keys)
+    for(int i=0; i<size; i++) {
+      k[i]=keys[i];
+      v[i]=values[i];
+    }
+    // update references to keys and values instance arrays
+    keys=k;
+    values=v;
+  }
+}
