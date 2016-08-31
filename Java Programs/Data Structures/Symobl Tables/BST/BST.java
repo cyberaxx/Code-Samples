179f04f8714ac2d4df31d6d396c515e540502c3f
MT <cyberaxx@users.noreply.github.com>
2016-08-31 01:25:03 -0700

    BST skeleton implemented

--- Java Programs/Data Structures/Symobl Tables/BST/BST.java
+++ Java Programs/Data Structures/Symobl Tables/BST/BST.java
@@ -24,4 +24,46 @@
 
 */
 
+public class BST<Key extends Comparable<Key>, Value> {
+  // instance variables:
+  private Node root; // A BST characterize by a reference to its root node
 
+  // inner class that defines nodes of the LinkList that maintain key-value pairs
+  private class Node {
+    // instance fields:
+    // each node contains: key, value, link to nodes with keys less than its key (left), link to nodes with keys grater than its key (right)
+    private Key key; // a generic type key (with a total order implemented by a compareTo method)
+    private Value value; // a generic non-null value
+    // links to left and right subtrees (nodes with keys less than and greater than its own key;
+    private Node left;
+    private Node right;
+    // number of nodes in the BST rooted at the node instance:
+    private int count;
+  }
+
+   // BST Constructor
+   public BST(){root=new Node();}
+
+
+   // API:
+   // Symbol Table basic Operations: put, get, contains, delete, size, isEmpty, keys
+
+   // insertion: an insertion to a Symbol Table is always predeceased by a search to prevent duplicate keys (same keys associated to different values -> Associative Array Abstraction violation!)
+   public void put(Key key, Value value) {
+     /* BST is Binary Tree (because comparison is a binary relation with a binary outcome (less than or greater than), 
+        and Binary Trees are RECURSIVE data structtures (empty or a node with links to left and right binary subtrees)
+        to modify the structure of BST we use private helper methods that take a copy of reference to the root as an input
+        argument and go over the BST change its strucrure and pass on the changes back to the root node and all nodes in the path
+     */
+     // Sanity check of input arguments:
+     if(key==null || value==null) throw new NullPointerException();
+     root=put(root, key, value);
+   }
+
+   // private recursive helper method that takes a reference to the root of subtree and key and value to be inserted to the subtree rooted at node
+   private Node put(Node node, Key key, Value value) {
+
+
+   return null;
+   }
+}
