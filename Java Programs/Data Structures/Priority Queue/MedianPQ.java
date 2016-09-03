a019c5a50d2f74b0443cd8fe1307ffaf664639d8
MT <cyberaxx@users.noreply.github.com>
2016-09-03 12:56:06 -0700

    MedianPQ.java implemented

--- Java Programs/Data Structures/Priority Queue/MedianPQ.java
+++ Java Programs/Data Structures/Priority Queue/MedianPQ.java
@@ -2,111 +2,132 @@
 Dynamic-median finding:
 Design a data type that supports insert in logarithmic time, find the median in constant time, and remove the median in logarithmic time.
 
-Solution. Keep the median key in v; 
-use a max-oriented heap for keys less than the key of v; 
-use a min-oriented heap for keys greater than the key of v. 
-To insert, add the new key into the appropriate heap, replace v with the key extracted from that heap.
-
 http://algs4.cs.princeton.edu/24pq/
 */
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+/*
+  MedianPQ data type is a generalization of collection data type where keys are generic comparable types. It uses the total ordering of keys
+  in order to organize keys in the data structure such that the median of the keys can be accessed at constant time O(1) (generalization of Queue data type)
+  
+  Collection data type to maintain keys of MedianPQ data structure:
+  To implement this data structure, instead of implicit array implementation of binary heap (that have been used in MinPQ and MaxPQ) we need a more
+  sophistacate collection data type.
+  We need to use one min oriented and one max oriented priority queue to maintain keys of MedianPQ:
+	1. Maintain a collection of M largest keys LESS than the median key: MinPQ (efficiently filtering out (extract) MIN key repeatedly)
+	2. Maintain a collection of M smallest keys GREATER than the medain key: MaxPQ (efficiently filtering out (extract) MAX key repeatedly)
+	3. Data Structue invariance:
+	     i. Median is always LESS than the head of MinPQ of keys with greater values
+             ii. Median is always GREATER than the head of MaxPQ of keys with less values
 
+        The invariance has to preserved after any sequence of insertion/deletion (structural changes to the data structure)
+	The head of MinPQ gKeys and MaxPQ lKeys are two candidates to be the next MEDIAN after any insertion and deletion to preseve the invariance of DS
+*/
+
+import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.MaxPQ;
 
-public class MedianPQ<Key extends Comparable<Key>> implements Iterable<Key> {
-  // instance field of MedianPQ:
-  private MinPQ<Key> minPQ; // collection of keys GREATER than the median
-  private MaxPQ<Key> maxPQ; // collection of keys LESS than the median
+public class MedianPQ<Key extends Comparable<Key>> {
+
+  // instance variables:
+  private MinPQ<Key> gKeys; // min oriented priority queue of keys GREATER than the median
+  private MaxPQ<Key> lKeys; // max oriented priority queue of keys LESS than the median
   private Key median;
 
-  // Constructor
+  // constructor:
   public MedianPQ() {
-    minPQ=new MinPQ<Key>(); // empty min oriented pq
-    maxPQ=new MaxPQ<Key>(); // empty max oriented pq
+    // initialize instance variables:
+    gKeys=new MinPQ<Key>(); // empty min oriented priority queue for generic comparable type Key
+    lKeys=new MaxPQ<Key>(); // empty collection of key in a form of max oriented priority queue
     median=null;
   }
 
-  // API:
-  public void insert(Key key){
-    // sanity check the key:
-    if(key==null) throw new NullPointerException();
-    // if MedianPQ is empty
-    if(isEmpty()) {
-      // set the median:
-      median=key;
-      // insert the key to the max oriented heap (arbitary):
-      maxPQ.insert(key);
-      // the extracted max is the LARGEST key in Median PQ which is less than previous median key
-    }
-    // if MedianPQ is not empty
-    else {
-      // compare the new key with median:
-      int cmp=key.compareTo(median);
-      // use a max-oriented heap for keys less than the key of v:
-      // MaxPQ would maintain the collection keys LESS than v 
-      if(cmp<0) {
-	// To insert, add the new key into the appropriate heap:
-	maxPQ.insert(key);
-	// replace median with the key extracted from that heap:
-        // filter out MAX elements from collection of keys LESS than the median key
-        maxExch();
-      }
-      // use a min-oriented heap for keys greater than the key of v: filter out min elements from the 
-      // collection of items greater than median
-      else {
-	// To insert, add the new key into the appropriate heap:
-	minPQ.insert(key);
-	// replace median with the key extracted from that heap:
-        // filter out MIN elements from collection of keys GREATER than the median key
-        minExch();
-	// the extract min is the SMALLEST key in MedianPQ which is greater than previous median
-      }
-    }
+  // API: instance methods of MedianPQ data type
+  // is MedianPQ instance empty
+  public boolean isEmpty() {return median==null;}
+  // number of keys in MedianPQ instance
+  public int size() {
+    if(isEmpty()) return 0; // empty MedianPQ
+    // sum of number of items in Min and Max pqs and the median key
+    return gKeys.size()+lKeys.size()+1;
   }
-
-  // delete the median
-  public Key delMedian() {
-    if(isEmpty()) throw new NoSuchElementException();
-    Key key=median;
-    // take the max key from collection of keys less than median to replace it with
-    median=maxPQ.delMax();
-    return key;
-  }
-
-  // return median O(1)
-  public Key median() {
+  // return the median in O(1)
+  public Key median(){
     if(isEmpty()) throw new NoSuchElementException();
     return median;
   }
-  // the MedianPQ is empty if both its pqs were empty:
-  public boolean isEmpty(){return minPQ.isEmpty()&&maxPQ.isEmpty();}
-  // number of elements is the sum of number elements in both pqs 
-  public int size(){return minPQ.size()+maxPQ.size();}
 
+  // Insertion and deletion: 
+  // PRESERVE MedianPQ INVARIANCE: median must be within [MaxPQ.head()  MinPQ.head()] range  
+  public void insert(Key key) {
+    // sanity check the key
+    if(key==null) throw new NullPointerException();
 
-  // Helper methods:
-  private void maxExch() {
-    Key temp=maxPQ.delMax();
-    maxPQ.insert(median);
-    median=temp;
+    // if the median priority queue is empty:
+    if(isEmpty()) {median=key; return ;}// set the first key as the median
+    
+    // Otherwise:
+    // 1. Compare the new key with the current median:
+    int cmp=key.compareTo(median);
+
+    /* 2. if the new key is GREATER than the median:
+          i. median has to get UPDATED.
+          ii. new median has to be GREATER than the previous one
+
+	  to accomplish this:
+	a. add the new key to the MinPQ of keys greater than the median
+	b. delMin() extract the min key from the collection of keys GREATER than median
+	c. replace that key with the old median
+	d. add the old median to the MaxPQ of keys less than the median
+    */
+
+    Key temp;
+    if(cmp>0) {
+      // a. insert the new key to the MinPQ of keys GREATER than median
+      gKeys.insert(key);
+      // b. extract the Min key from the MinPQ of keys GREATER than median
+      // c. replace the extracted key with the median
+      temp=median;
+      median=gKeys.delMin();
+      // d. insert the old median to the MaxPQ of keys less than the median
+      lKeys.insert(temp);
+    }
+
+    /* 3. if the new key is LESS than the median:
+          i. median has to get UPDATED.
+          ii. new median has to be LESS than the previous one
+
+	  to accomplish this:
+	a. add the new key to the collection of keys less than the median
+	b. extract the max key from the collection of keys less than the median
+	c. replace the median with the extracted key
+	d. add the old median to the collection of keys greater than the median
+    */
+    if(cmp<0){
+      // a. add the new key to MaxPQ of keys less than the median
+      lKeys.insert(key);
+      // b. remve the max key from the MaxPQ of keys less than the median
+      // c. replace the median with the extracted key
+      temp=median;
+      median=lKeys.delMax();
+      // d. add the old median to the collection of keys greater than the median
+      gKeys.insert(temp); 
+    }
+
+    /* 4. if the new key is EQUAL to the median: Do NOT support duplicate keys
+          i. median stays the same.
+          ii. Do nothing
+    */
+
   }
-  private void minExch() {
-    Key temp=minPQ.delMin();
-    minPQ.insert(median);
-    median=temp;
-  }
-
-  @Override
-  public Iterator<Key> iterator(){return null;}
 
   // test client:
   public static void main(String[] args) {
-    MedianPQ<Integer> mpq=new MedianPQ<Integer>();
-    for(int i=-11; i<11; i++) mpq.insert(i);
-    System.out.println("Median: " + mpq.median());
+    MedianPQ<Integer> pq=new MedianPQ<Integer>();
+    for(int i=0; i<5; i++) pq.insert(i);
+    System.out.println("Median: " + pq.median());
+   System.out.println("Size: " + pq.size());
+   System.out.println("Empty? " + pq.isEmpty());
     System.out.println();
   }
 
