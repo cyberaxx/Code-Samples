4e66701f195314e835794eefb6dd86b1b3095f00
MT <cyberaxx@users.noreply.github.com>
2016-09-03 15:03:12 -0700

    BSTReconstruction.java added to BST repo

---
+++ Java Programs/Data Structures/Symobl Tables/BST/BSTReconstruction.java
@@ -0,0 +1,22 @@
+/* 
+BST reconstruction. 
+Given the preorder traversal of a BST (not including null nodes), reconstruct the tree.
+*/
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.ArrayList;
+
+public class BSTreconstruction {
+
+  public static Iterable<Integer> bst(Deque<Integer> preOrder) {
+    // preorder traversal: Data L-subtree R-subtree
+    return null;
+  }
+
+  public static void main(String[] args) {    
+
+  }
+
+}
