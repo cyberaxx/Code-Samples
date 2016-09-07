e9ef0a9ff121f1361d87262481c1133f9c23129c
MT <cyberaxx@users.noreply.github.com>
2016-09-07 12:40:54 -0700

    LineGraph.java simple DP added to DP repo

--- Java Programs/Dynamic Programming/LineGraph.java
+++ Java Programs/Dynamic Programming/LineGraph.java
@@ -1,9 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class LineGraph {
 
-  public static int maxWIS(int [] weights) {
+  public static int maxWIS(int [] weights, List<Integer> path) {
     // number of vertices of the graph
     int n=weights.length;
-
     // A memo table to keep track of the value of the optimal solution to subproblems (optimal substructure)
     // of the original problem:
     int[] memo=new int[n+1]; // considering subproblems in form of prefixes of 1....N sequence of nodes
@@ -17,16 +19,22 @@ public class LineGraph {
       // if we are considering subgraphs with less than two nodes:
       if(i<2) memo[i]=weights[i-1]; // there is no other way rather than adding this single node to the max IS
       else // recursively choose the best choice among TWO possible optimla substructure:
-       memo[i]=Math.max(memo[i-2]+weights[i-1], memo[i-1]);
+        memo[i]=Math.max(memo[i-2]+weights[i-1], memo[i-1]);
     }
+
+    // path reconstruction:
+    for(int i=n; i>=2; i--)
+      if(memo[i]==memo[i-2]+weights[i-1]) {path.add(i-1); i--;}
+
     return memo[n];
   }
 
   public static void main(String[] args) {
     int [] weights=new int [args.length];
-    for(int i=0; i<weights.length; i++) {
+    List<Integer> path=new ArrayList<Integer>(); // an empty list instance
+    for(int i=0; i<weights.length; i++)
       weights[i]=Integer.parseInt(args[i]);
-    }
-    System.out.println("MAX WIS: "+maxWIS(weights));
+    System.out.println("MAX WIS: "+maxWIS(weights, path));
+    System.out.println("MAX WIS vertices: "+path);
   }
 }
