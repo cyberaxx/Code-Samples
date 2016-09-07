1d478bc3d8dcd8ca4d898c9bbc701375c8833072
MT <cyberaxx@users.noreply.github.com>
2016-09-07 11:55:39 -0700

    LineGraph.java simple DP added to DP repo

---
+++ Java Programs/Dynamic Programming/LineGraph.java
@@ -0,0 +1,32 @@
+public class LineGraph {
+
+  public static int maxWIS(int [] weights) {
+    // number of vertices of the graph
+    int n=weights.length;
+
+    // A memo table to keep track of the value of the optimal solution to subproblems (optimal substructure)
+    // of the original problem:
+    int[] memo=new int[n+1]; // considering subproblems in form of prefixes of 1....N sequence of nodes
+
+    // base case: 0 node graph
+    memo[0]=0; // max weight of IS for 0 node
+
+    // Systematically solve optimal subtructures (in this case prefixes of 1...n) 
+    // to be able to solve the original problem of size n -> the solution would be at memo[n]
+    for(int i=1; i<=n; i++) {
+      // if we are considering subgraphs with less than two nodes:
+      if(i<2) memo[i]=weights[i-1]; // there is no other way rather than adding this single node to the max IS
+      else // recursively choose the best choice among TWO possible optimla substructure:
+       memo[i]=Math.max(memo[i-2]+weights[i-1], memo[i-1]);
+    }
+    return memo[n];
+  }
+
+  public static void main(String[] args) {
+    int [] weights=new int [args.length];
+    for(int i=0; i<weights.length; i++) {
+      weights[i]=Integer.parseInt(args[i]);
+    }
+    System.out.println("MAX WIS: "+maxWIS(weights));
+  }
+}
