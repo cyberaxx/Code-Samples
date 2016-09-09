36be9cb395c22c9feeff234e42c292276d6b6233
MT <cyberaxx@users.noreply.github.com>
2016-09-09 06:57:04 -0700

    PalindromeChecker.java added to stack applications

--- Java Programs/Data Structures/Stack/PalindromeChecker.java
+++ Java Programs/Data Structures/Stack/PalindromeChecker.java
@@ -1,36 +1,37 @@
-/*
-Palindrome checker. 
-Write a program that reads in a sequence of strings and checks whether it constitutes a palindrome. 
-Ignore punctuation and spaces and case. (A MAN, A PLAN, A CANAL - PANAMA). Use one stack and one queue.
-*/
-
 import java.util.Deque;
 import java.util.ArrayDeque;
 
 public class PalindromeChecker {
-  public static boolean palindrome(String input) {
-    Deque<Character> stack=new ArrayDeque<Character>();
-    Deque<Character> queue=new ArrayDeque<Character>();
 
-    // convert to lowercase:
-    String string=st.trim().toLowerCase();
+  public static boolean isPalindrome(String s) {
+    // trivial cases: empty string and a single character string:
+    if(s==null) return true;
+    if(s.length()<2) return true;
 
-    // Read string from input:
-    for(Character ch:string) {
-      // ignore punctuation and space
-      if(ch==' ' || ch==',' || ch=='.' || ch==';' || ch=='-' || ch=='!' || ch=='?') continue;
-        stack.push(ch);
-	queue.offer(ch);
+    Deque<Character> stack=new ArrayDeque<Character>(); // empty stack of characters
+    Deque<Character> q=new ArrayDeque<Character>(); // empty queue of characters
+
+    for(int i=0; i<s.length(); i++) {
+      stack.push(s.charAt(i)); // push the current character to the stack
+      q.offer(s.charAt(i)); // enqueue the current character to the queue
     }
 
-    while(!stack.isEmpty() && !queueu.isEmpty){
-    
+    // compare characters:
+    while(!q.isEmpty() && !stack.isEmpty()) {
+      // remove the head of queue and top of the stack and compare them
+      // with each other:
+      if(q.poll()!=stack.pop())
+	return false;
     }
-    
+    return true;
   }
 
+  // test client:
   public static void main(String[] args) {
-    System.out.println(palindrome("A MAN, A PLAN, A CANAL - PANAMA"));
+    System.out.println(isPalindrome("absmcdcmsba"));
+    System.out.println(isPalindrome("aaaaaaaaaaaaaaaaaa"));
+    System.out.println(isPalindrome("a"));
+    System.out.println(isPalindrome(""));
   }
 
 }
