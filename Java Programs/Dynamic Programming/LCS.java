8dca14f44df0f717e27b345f5aaa0838c5192104
MT <cyberaxx@users.noreply.github.com>
2016-09-10 20:26:25 -0700

    LCS.java edited

---
+++ Java Programs/Dynamic Programming/LCS.java
@@ -0,0 +1,9 @@
+/*
+  The longest common subsequence (LCS) problem is the problem of finding the longest subsequence common
+  to all sequences in a set of sequences (often just two sequences).
+
+  Related problem:
+  The longest common *substring* problem is to find the longest string (or strings) that is a 
+  substring (or are substrings) of two or more strings.
+*/
+
