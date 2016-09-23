c953134f0cf5e4058f4a4b496a7299291f71977a
MT <cyberaxx@users.noreply.github.com>
2016-09-22 19:54:15 -0700

    LCS.java longest common subsequence based on alignment

--- Java Programs/Dynamic Programming/LCS.java
+++ Java Programs/Dynamic Programming/LCS.java
@@ -6,6 +6,11 @@
   The longest common *substring* problem is to find the longest string (or strings) that is a 
   substring (or are substrings) of two or more strings.
 */
+
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class LCS {
   /* Similar to edit distance problem:
      1. Find the optimal alignment (alignment with min number of gap insertions)
@@ -14,8 +19,23 @@ public class LCS {
   */
 
   // return the length of lcs and reconstruct the lcs as well
-  public static int lcs(String x, String y) {
+  public static int lcs(String x, String y, String str) {
+    // consider two strings of input as two sequences of characters
+    int N=x.length();
+    int M=y.length();
+
+    /*
+      Subproblems (state space): 
+	prefixes of x and y (optimal substructure would be lcs of all possible subsequences of x and y)
+      Choices (implicit transitions between states):
+      	1. insert a gap into x (+1)
+      	2. insert a gap into y (+1)
+      	3. match (if possible) (0)
+    */
 
     return 0;
+  }
+
+  public static void main(String[] args) {
   }
 }
