cb63ef8b55881d2815dfd511ac654bd7088043a5
MT <cyberaxx@users.noreply.github.com>
2016-09-23 23:39:11 -0700

    BFS Applications added to repo

---
+++ Java Programs/Data Structures/Graphs/Undirected Graphs/BFS Applications/SP.java
@@ -0,0 +1,10 @@
+/*
+  Shortest path in complement graph. 
+  Given a graph G, design an algorithm to find the shortest path (number of edges) between s and every other vertex in the complement graph G'. 
+  The complement graph contains the same vertices as G but includes an edge v-w if and only if the edge v-w is not in G. 
+  Can you do any better than explicitly computing the complement graph G' and running BFS in G'?
+*/
+
+public class SP {
+
+}
---
+++ Java Programs/Data Structures/Graphs/Undirected Graphs/BFS Applications/SpanningTree.java
@@ -0,0 +1,5 @@
+/*
+  Spanning tree. 
+  Design an algorithm that computes a spanning tree of a connected graph is time proportional to V + E. 
+  Hint: use either BFS or DFS.
+*/
