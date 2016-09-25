a3ad65d219fe159b8cc526ce92d413e560048254
MT <cyberaxx@users.noreply.github.com>
2016-09-24 20:44:41 -0700

    CatalanNumbers.java BU approach to compute catalan numbers

---
+++ Java Programs/Dynamic Programming/CatalanNumbers.java
@@ -0,0 +1,34 @@
+/*
+
+  In combinatorial mathematics, the Catalan numbers form a sequence of 
+  natural numbers that occur in various counting problems, often involving 
+  recursively-defined objects.
+
+  The Catalan numbers satisfy the recurrence relation:
+  c[0]=1
+  c[n+1]=SUM(c[i]*c[n-i]) (For i=0 to n) for n>=0
+
+
+  Catalan Number Applications:
+  1. C[n] counts the number of expressions containing n pairs of parentheses which are correctly matched.
+  2. C[n] is the number of different ways n + 1 factors can be completely parenthesized (or the number of ways of associating n applications of a binary operator).
+  3. C[n] is the number of rooted binary trees with n internal nodes (n + 1 leaves or external nodes).
+
+*/
+
+
+
+public class CatalanNumbers {
+
+  // DP implementation of Catalan recurrence (Similar to Fibonacci)
+  private static int catalan(int n) {
+
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+
+}
