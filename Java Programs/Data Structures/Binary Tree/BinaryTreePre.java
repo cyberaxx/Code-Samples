f76842f61d82b7a49db6d2c289baeba72b90569e
MT <cyberaxx@users.noreply.github.com>
2016-09-29 08:43:09 -0700

    iterative preorder

--- Java Programs/Data Structures/Binary Tree/BinaryTreePre.java
+++ Java Programs/Data Structures/Binary Tree/BinaryTreePre.java
@@ -9,45 +9,26 @@
  */
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-	// an empty list for node keys
-        List<Integer> list=new ArrayList<Integer>(); 
+        List<Integer> list=new ArrayList<Integer>(); // an empty list node keys
         // if tree is empty return the list
         if(root==null) return list;
-
+        
         // Otherwise:
         // Recursion stack of TreeNodes
         Deque<TreeNode> stack=new ArrayDeque<TreeNode>();
-        // A set of TreeNodes to keep track of marked nodes
-        HashSet<TreeNode> marked=new HashSet<TreeNode>();
+        TreeNode x=root;
         
-        // starting from the root:
-        // add root to the recursive stack (first recursive call on root)
-        stack.push(root);
-        
-        // while recursive stack is not empty
-        while(!stack.isEmpty()) {
-            // sneek peek at the top of the stack (color it gray)
-            TreeNode x=stack.peek();
-            
-	    /* D */
-            // if x has not been visited already mark x as visited
-            if(!marked.contains(x)) {
-                marked.add(x);
+        while(x!=null || !stack.isEmpty()) {
+            if(x==null) {
+                x=stack.pop();
+                x=x.right;
+            }
+            else {
                 list.add(x.val);
+                stack.push(x);
+                x=x.left;
             }
-            
-            // recursively visite x's l and r child
-	    /* L */ //---> Deep LEFT
-            if(x.left!=null && !marked.contains(x.left)) {
-                stack.push(x.left);
-            }
-	    /* R */ //---> if not possible to go LEFT any further, then right
-            else if(x.right!=null && !marked.contains(x.right)) {
-                stack.push(x.right);
-            }
-            else stack.pop();
         }
-  
         return list;
     }
 
