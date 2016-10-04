8d772897b943e6739954cb3563dd2d2246ef84ee
MT <cyberaxx@users.noreply.github.com>
2016-10-03 19:31:26 -0700

    circular doubly linked list #loopylooplopp

--- Java Programs/Data Structures/LinkedList/CList.java
+++ Java Programs/Data Structures/LinkedList/CList.java
@@ -7,7 +7,7 @@
 
 import java.util.*;
 
-public class CList<Item> {
+public class CList<Item> implements Iterable<Item>{
   // instance field: a reference to a sentinel node:
   private Node<Item> sentinel;
   private int size;
@@ -15,24 +15,37 @@ public class CList<Item> {
   // Constructor:
   public CList() {
      sentinel=new Node<Item>();
-     sentinel.next=sentinel.prev;
+     sentinel.next=sentinel;
      sentinel.prev=sentinel;
      size=0;
   }
 
   // API: behaviours (addFirst, addLast, removeFirst, removeLast, contains, remove, size, isEmpty) 
   public int size(){return size;}
-  public boolean isEmpty(){return sentinel.next==sentinel.prev;}
+  public boolean isEmpty(){return sentinel.next==null;}
   public void addFirst(Item item) {
     // Create a new node with the given item:
     Node<Item> node=new Node<Item>(item);
 
     // 1. check if the CList instance is empty:
     if(isEmpty()) {
+      sentinel.next=node;
+      sentinel.prev=node;
+      node.prev=sentinel;
+      node.next=sentinel;
+      size++;
+      return ;
     }
     // 2. if not:
     // copy a reference to the current first:
     Node<Item> oldFirst=sentinel.next;
+    node.prev=sentinel;
+    node.next=oldFirst;
+    sentinel.next=node;
+    oldFirst.prev=node;
+
+    size++;
+    return ;
   }
 
   public void addLast(Item item){
@@ -62,9 +75,25 @@ public class CList<Item> {
     }
   }
 
+  // LoopyLoopLoopLoopyLoopLoopLoopyLoopLoopLoopyLoopLoop Iterator!
+  @Override
+  public Iterator<Item> iterator(){return new ListIterator();}
+  private class ListIterator implements Iterator<Item> {
+    private Node<Item> current;
+    // Constructor:
+    public ListIterator() {current=sentinel;}
+    @Override
+    public boolean hasNext(){return current!=null;}
+    @Override
+    public Item next(){
+      if(!hasNext()) throw new NoSuchElementException();
+      // advance the current:
+      current=current.next;
+      return current.item;
+    }
+  }
+
   public static void main(String[] args) {
-    CList<Integer> l=new CList<Integer>();
-    l.addFirst(5); 
   }
 }
 
