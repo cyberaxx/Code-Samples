f541f2621264144e2371fc56cdab5c58ee7455c6
CYBERAX <cyberaxx@users.noreply.github.com>
2016-10-17 01:16:18 -0700

    String key symbol table implementation

--- Java Programs/String/Search/TrieST.java
+++ Java Programs/String/Search/TrieST.java
@@ -131,12 +131,13 @@ public class TrieST<Value> {
     Node x=get(root, prefix, 0);
     // traverse the trie rooted at node x:
     Deque<String> q=new ArrayDeque<String>();
-    return keys(x, "", q);
+    keys(x, "", q);
+    return q;
   }
   
   // longest prefix of the given string in the collection of strings:
   public String longestPrefixOf(String key) {
-    
+    return null;
   }
   
 
