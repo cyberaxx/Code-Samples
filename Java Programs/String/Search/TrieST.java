5a27dbf5cad60c8ae3fa8235f74e5d16304b1de2
CYBERAX <cyberaxx@users.noreply.github.com>
2016-10-17 01:04:47 -0700

    String key symbol table implementation

--- Java Programs/String/Search/TrieST.java
+++ Java Programs/String/Search/TrieST.java
@@ -69,13 +69,61 @@ public class TrieST<Value> {
   public boolean contains(String key){return get(key)!=null;}
   
   public void remove(String key) {
-  
+    // check if the key is null
+    if(key==null) throw new NullPointerException();
+    
+    // search for the given key in the R-way trie, and remove the value associated to it
+    root=remove(root, key, 0);  
   }
+  // private recursive helper method:
+  private Node remove(Node x, String key, int d) {
+    // termination condition:
+    // failed to find such a key in the trie
+    if(x==null) throw new NoSuchElementException();
+    if(d==key.length()){x.value=null;} // set the value associated with the key to null
+    
+    // Otherwise: search for the given key by looking for its next character:
+    char c=key.charAt(d);
+    x.next[c]=remove(x.next[c], key, d+1);
+    
+    // On the way back up if node x is null remove it:
+    if(x.value!=null) return x;
+    // if x.value is null, check if next is null for all symbols in alphabet:
+    for(char ch=0; ch<R; ch++) {
+      if(x.next[ch]!=null) return x;
+    }
+    return null;
+  }
+  
   public int size(){return size;}
   public boolean isEmpty(){return root==null;}
   
   // iterate over trie keys in sorted order:
-  public Iterable<String> keys(){return null;}
+  public Iterable<String> keys() {
+    // perform inorder traversal of the trie for all R possible symbols from left to right:
+    Deque<String> q=new ArrayDeque<String>(); // empty queue
+    String key="";
+    // start off from the root node to all leaves from left to right:
+    keys(root, key, q);
+    
+    // retrun the Iterable:
+    return q;
+  }
+  
+  // private recursive trie traversal in sorted order:
+  private void keys(Node x, String key, Deque<String> q) {
+    // termination conditon:
+    if(x==null) return ;
+    
+    // from left to right
+    for(char c=0; c<R; c++) {
+      if(x.next[c]!=null) {
+        keys(x.next[c], key+c, q);
+        // at the leaf node:
+        if(x.value!=null) q.offer(key);
+      }
+    }  
+  }
 
 }
 
